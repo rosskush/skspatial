002e25628101d8fd9bad4861f7e89a6d6b633f8a
rosskush <ross.kush@gmail.com>
2019-07-25 10:20:55 -0500

    added extract_raster function

--- skspatial/skspatial.py
+++ skspatial/skspatial.py
@@ -224,6 +224,16 @@ class interp2d():
         fig.tight_layout()
         return axes
 
+def extract_raster(raster_path,xy):
+    # raster_path : path to raster
+    # xy: list or array of tuples of x,y i.e [(x1,y1),(x2,y2)...(xn,yn)]
+    # returns list of length xy or sampled values
+    raster = rasterio.open(raster_path)
+    values = list(raster.sample(xy)) # convert generator object to list
+
+    values = [item[0] for item in values] # list comprehension to get the value
+    return values
+
 if __name__ == '__main__':
     # for testing only
     import os
