8537e02e194af7e3ae2a625e3e19f43ec4722700
Ross Kushnereit <30669053+rosskush@users.noreply.github.com>
2020-08-05 11:19:44 -0500

    Added "in memory" way to clip raster
    Don't have to read/write clipped raster

--- skspatial/utils/clip_raster.py
+++ skspatial/utils/clip_raster.py
@@ -3,6 +3,8 @@ import geopandas as gdf
 import rasterio
 import numpy as np
 import os
+from rasterio import mask
+from rasterio.io import MemoryFile
 
 def getFeatures(gdf):
     import json
@@ -23,7 +25,7 @@ def clip_raster(rasterobj, clip_obj, path):
     coords = getFeatures(clip_obj)
 
     # print(coords)
-    out_img, out_transform = mask(dataset=rasterobj, shapes=coords, crop=True)
+    out_img, out_transform = mask.mask(dataset=rasterobj, shapes=coords, crop=True)
     # print(out_img)
     out_img = np.array(out_img[0])
     new_dataset = rasterio.open(os.path.join(path), 'w',
@@ -33,4 +35,24 @@ def clip_raster(rasterobj, clip_obj, path):
 
     new_dataset.write(out_img, 1)
     new_dataset.close()
+    return new_dataset
+
+def get_clip_raster(rasterobj, clip_obj):
+    '''
+    rasterobj - rasterio.read()
+    clip_obj - clip shapefile, make sure there is only 1 geometry
+
+    '''
+    coords = getFeatures(clip_obj)
+
+    out_img, out_transform = mask.mask(dataset=rasterobj, shapes=coords, crop=True)
+    out_img = np.array(out_img[0])
+
+    with MemoryFile() as memfile:
+        meta = {"count": 1, "width": out_img.shape[1], "height": out_img.shape[0], "transform": out_transform,
+                "nodata": np.nan, "dtype": out_img.dtype,'crs':clip_obj.crs}
+        with memfile.open(driver='GTiff', **meta) as new_dataset:
+            new_dataset.write(out_img)
+
+
     return new_dataset
