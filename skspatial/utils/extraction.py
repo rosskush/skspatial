cbf448d8027666d5df57c2612fbce7e9adedd511
Ross Kushnereit <30669053+rosskush@users.noreply.github.com>
2020-08-21 08:11:24 -0500

    raster2pts
    Add utility for extracting raster to points based on the centroids of the raster

--- skspatial/utils/extraction.py
+++ skspatial/utils/extraction.py
@@ -1,5 +1,6 @@
 import rasterio
-
+import geopandas as gp
+from shapely.geometry import Point
 
 
 def extract_raster(raster_path,xy):
@@ -11,3 +12,24 @@ def extract_raster(raster_path,xy):
 
     values = [item[0] for item in values] # list comprehension to get the value
     return values
+
+def raster2pts(rasobj, column='Value'):
+    crs = rasobj.crs
+    array = rasobj.read(1)
+
+    nrow, ncol = rasobj.height, rasobj.width
+
+    data = []
+    i = 0
+    for r in range(nrow):
+        for c in range(ncol):
+
+            x,y = rasobj.xy(r,c)
+            z = array[r,c]
+            data.append([z,Point(x,y)])
+
+            i+=1
+
+    gdf = gpd.GeoDataFrame(data,columns=[column,'geometry'],crs=crs)
+
+    return gdf
