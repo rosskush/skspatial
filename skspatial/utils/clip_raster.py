7af359ef1168aa20bd68f4cff6a5501013744a0b
rosskush <ross.kush@gmail.com>
2021-04-02 09:21:09 -0500

    added crop

--- skspatial/utils/clip_raster.py
+++ skspatial/utils/clip_raster.py
@@ -13,10 +13,7 @@ def getFeatures(gdf):
     return [json.loads(gdf.to_json())['features'][0]['geometry']]
     # return [json.loads(gdf.loc[gdf['geometry'].area == gdf['geometry'].area.max()].to_json())['features'][0]['geometry']]
 
-
-
-
-def clip_raster(rasterobj, clip_obj, path):
+def clip_raster(rasterobj, clip_obj, path,crop=True):
     '''
     rasterobj - rasterio.read()
     clip_obj - clip shapefile, make sure there is only 1 geometry
@@ -25,9 +22,11 @@ def clip_raster(rasterobj, clip_obj, path):
     coords = getFeatures(clip_obj)
 
     # print(coords)
-    out_img, out_transform = mask.mask(dataset=rasterobj, shapes=coords, crop=True)
+    out_img, out_transform = mask.mask(dataset=rasterobj, shapes=coords, crop=crop)
     # print(out_img)
     out_img = np.array(out_img[0])
+    out_img[out_img == rasterobj.nodata] = np.nan
+
     new_dataset = rasterio.open(os.path.join(path), 'w',
                                 driver='GTiff', height=out_img.shape[0],
                                 width=out_img.shape[1], count=1, dtype=out_img.dtype, crs=clip_obj.crs,
@@ -37,7 +36,7 @@ def clip_raster(rasterobj, clip_obj, path):
     new_dataset.close()
     return new_dataset
 
-def get_clip_raster(rasterobj, clip_obj):
+def get_clip_raster(rasterobj, clip_obj, crop=True):
     '''
     rasterobj - rasterio.read()
     clip_obj - clip shapefile, make sure there is only 1 geometry
@@ -45,9 +44,9 @@ def get_clip_raster(rasterobj, clip_obj):
     '''
     coords = getFeatures(clip_obj)
 
-    out_img, out_transform = mask.mask(dataset=rasterobj, shapes=coords, crop=True)
+    out_img, out_transform = mask.mask(dataset=rasterobj, shapes=coords, crop=crop)
     out_img = np.array(out_img[0])
-
+    out_img[out_img == rasterobj.nodata] = np.nan
     with MemoryFile() as memfile:
         meta = {"count": 1, "width": out_img.shape[1], "height": out_img.shape[0], "transform": out_transform,
                 "nodata": np.nan, "dtype": out_img.dtype,'crs':clip_obj.crs}
@@ -56,3 +55,19 @@ def get_clip_raster(rasterobj, clip_obj):
 
 
     return new_dataset
+
+def clip_raster2array(rasterobj, clip_obj,crop=True):
+    '''
+        rasterobj - rasterio.read()
+        clip_obj - clip shapefile, make sure there is only 1 geometry
+
+        '''
+    coords = getFeatures(clip_obj)
+
+    # out_img, out_transform = mask.mask(dataset=rasterobj, shapes=coords, crop=True)
+    out_img, out_transform = mask.mask(dataset=rasterobj, shapes=coords, crop=crop)
+
+    out_img = np.array(out_img[0])
+    out_img[out_img == rasterobj.nodata] = np.nan
+
+    return out_img
